1f9642896166173eaeab6f265e0865cf028cbf70
Wes Horton <wrhorton592@gmail.com>
2025-07-17 16:25:50 -0700

    New lineplot for populations. Change functional line to allow for facet

--- R/functionalLinePlot.R
+++ R/functionalLinePlot.R
@@ -1,10 +1,49 @@
 functionalLine <- function(func_dt,
                            markerCol_v = "marker", markers_v, markerColors_v,
                            patientCol_v = "Patients", pt_v,
-                           classCol_v = "variable",
                            xVar_v = "Time point", yVar_v = "value", 
                            title_v) {
-  #' Prop and Func Bar
+  #' Functional Line
+  #' @description Line/spaghetti plot of functional expression
+  #' @param func_dt functional table
+  #' @param markerCol_v column for functional markers (should always be 'marker')
+  #' @param markers_v which markers to plot
+  #' @param markerColors_v color vector for marker color scale
+  #' @param patientCol_v column for patients (should always be 'Patients')
+  #' @param pt_v which patient to plot
+  #' @param xVar_v column to put on x-axis. Should be 'Time point'
+  #' @param yVar_v column to use for y-axis. Should be 'value'
+  #' @param title_v title for plot
+  #' @import data.table
+  #' @import ggplot2
+  #' @return ggplot
+  #' @export
+  
+  ### Subset data
+  subFunc_dt <- func_dt[get(patientCol_v) == pt_v & 
+                          get(markerCol_v) %in% markers_v,]
+  
+  ### Plots
+  plot_gg <- ggplot(data = subFunc_dt, aes(x = !!sym(xVar_v), y = !!sym(yVar_v), color = !!sym(markerCol_v), group = !!sym(markerCol_v))) +
+    geom_point(cex = 1.5) +
+    geom_line(size = 1.5) +
+    scale_color_manual(values = markerColors_v, breaks = names(markerColors_v)) +
+    #big_label(angleY_v = F) + angle_x() +
+    massive_label() +
+    ggtitle(title_v) +
+    labs(y = "Percent Functional", x = NULL)
+  
+  return(plot_gg)
+  
+} # Functional Line
+
+functionalLineFacet <- function(func_dt,
+                                markerCol_v = "marker", markers_v, markerColors_v,
+                                patientCol_v = "Patients", pt_v,
+                                classCol_v = "variable",
+                                xVar_v = "Time point", yVar_v = "value", 
+                                title_v) {
+  #' Functional Line Facet
   #' @description Line/spaghetti plot of functional expression
   #' @param func_dt functional table
   #' @param markerCol_v column for functional markers (should always be 'marker')
---
+++ R/popLinePlot.R
@@ -0,0 +1,37 @@
+popLine <- function(bar_dt,
+                    popCol_v = "variable", pop_v, 
+                    patientCol_v = "Patients", ptColors_v,
+                    xVar_v = "Time point", yVar_v = "value", 
+                    yLab_v = "Pct CD45",
+                    title_v) {
+  #' Prop and Func Bar
+  #' @description Line/spaghetti plot of functional expression
+  #' @param bar_dt barchart table (melted)
+  #' @param popCol_v column that holds populations. Should be 'variable' in melted data
+  #' @param pop_v which population to plot
+  #' @param markerColors_v color vector for marker color scale
+  #' @param patientCol_v column for patients (should always be 'Patients')
+  #' @param ptColors_v named color vector for patients
+  #' @param xVar_v column to put on x-axis. Should be 'Time point'
+  #' @param yVar_v column to use for y-axis. Should be 'value'
+  #' @param title_v title for plot
+  #' @import data.table
+  #' @import ggplot2
+  #' @return ggplot
+  #' @export
+  
+  ### Subset data
+  sub_dt <- bar_dt[get(popCol_v) %in% pop_v,]
+  
+  ### Plots
+  plot_gg <- ggplot(data = sub_dt, aes(x = !!sym(xVar_v), y = !!sym(yVar_v), color = !!sym(patientCol_v), group = !!sym(patientCol_v))) +
+    geom_point(cex = 1.5) +
+    geom_line(size = 1.5) +
+    scale_color_manual(values = ptColors_v, breaks = names(ptColors_v)) +
+    massive_label() +
+    ggtitle(title_v) +
+    labs(y = yLab_v, x = NULL)
+  
+  return(plot_gg)
+  
+} # popLine
